f3d6262115fa7a5358fcc31582731239b87ef97d
Caio Azevedo <cazevedo@hotmail.com>
2024-03-11 11:31:17 -0300

    Add files via upload

--- Rede_Neural.py
+++ Rede_Neural.py
@@ -48,7 +48,6 @@
 # %%
 import numpy as np
 import _pickle as pkl #utilizado para salvar/restaurar a rede
-import matplotlib.pyplot as plt
 #from sklearn.datasets import load_iris
 #from sklearn.datasets import make_blobs, make_circles, make_moons, make_classification
 #from sklearn.metrics import accuracy_score
