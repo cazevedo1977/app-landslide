b747879e868d500ba43a2944f907b69eb103b3dd
Caio Azevedo <cazevedo@hotmail.com>
2024-03-11 19:00:50 -0300

    Add files via upload

--- map.py
+++ map.py
@@ -38,10 +38,18 @@ APP_TITLE = 'Machine Learning for Landslide Susceptibility'
 APP_SUB_TITLE = 'From Theory to Practice: Machine Learning for Landslide Susceptibility Assessment in the Brazilian Atlantic Forest'
 
 
+#Get data with UTM coordinates and convert them to lat/long
+def getdata(fileName=None):
+    landslide_df = pd.read_csv(fileName)
+    zone_number = 23   #São Paulo coast UTM zone number
+    zone_letter = 'L'  #São Paulo coastUTM zone letter
+    landslide_df['lat'], landslide_df['lon'] = utm.to_latlon(easting=landslide_df['X'].values,northing= landslide_df['Y'].values,zone_number=zone_number,zone_letter=zone_letter)
+    
+    return landslide_df
 
 
 # Prepare and format data to perform prediction based on user selection
-def dataPreparation(dataSet, sample):
+def dataPreparation(dataSet):
     
     dataSet = dataSet[['slope','aspect','elevation','land_use','lithology','twi','curvature','class','lat','lon']]
     X = dataSet.drop('class',axis=1)
@@ -76,22 +84,35 @@ def dataPreparation(dataSet, sample):
     
     if st.session_state.sample == "train":
         dataSet = train_ds
+        X = train_x
+        y = train_y
     elif st.session_state.sample == "test":
         dataSet = test_ds
+        X = test_x
+        y = test_y
     elif st.session_state.sample == "validation":
         dataSet = val_ds
+        X = val_x
+        y = val_y
+    else:
+        X = np.concatenate((train_x, val_x))
+        X = np.concatenate((X, test_x))
+        y = np.concatenate((train_y, val_y))
+        y = np.concatenate((y, test_y))
+    
+    y = y.ravel() #convert that array shape to (n, ) (i.e. flatten it)    
 
     
-    return train_x, test_x, val_x, train_y, test_y, val_y, dataSet
+    return X, y, dataSet
 
-
+##### predictions functions ##################
 #Load trained ANN and predict landslide susceptibility 
 def run_prediction(dS,features):
     bestNN = None 
     bestNN = NeuralNetwork.load('customNN_guaruja_random.pkl')
     y_pred = bestNN.predict(features)
     dS['score'] = y_pred
-    dS['prediction'] = [int(b) for b in (y_pred > 0.5)]
+    dS['prediction'] = dS['score'].apply(lambda x: 1 if x > 0.5 else 0)
     return dS, y_pred
 
 
@@ -107,9 +128,27 @@ def features_importance(X, y):
     scores = np.round(scores, 2)
     return predictors, scores 
 
+# Compute and display performance metrics 
+def NetworkPerformance(y_real, y_prob):
+    y_predict = (y_prob > 0.5)
+    accuracy = accuracy_score(y_real, y_predict) # accuracy: (tp + tn) / (p + n)
+    #print('Accuracy: %f' % accuracy)
+    precision = precision_score(y_real, y_predict)     # precision tp / (tp + fp)
+    #print('Precision: %f' % precision)
+    recall = recall_score(y_real, y_predict) # recall: tp / (tp + fn)
+    #print('Recall: %f' % recall)
+    f1 = f1_score(y_real, y_predict) # f1: 2 tp / (2 tp + fp + fn)
+    #print('F1 score: %f' % f1)
+    # ROC AUC
+    auc = roc_auc_score(y_real, y_prob)
+    #print('ROC AUC: %f' % auc)
+    # confusion matrix
+    matrix = confusion_matrix(y_real, y_predict)
+    return accuracy, recall, auc 
 
+##### end predictions functions ##################
 
-##### user interface functions ##################
+##### sidebar user interface functions ##################
 
 def file_selector(folder_path='.\data'):
     filenames = os.listdir(folder_path)
@@ -119,8 +158,6 @@ def file_selector(folder_path='.\data'):
 def display_user_interaction():
     filename = file_selector()
     #st.write('You selected `%s`' % filename)
-    if "sample" not in st.session_state:
-        st.session_state.sample = "train"
 
     option = st.sidebar.selectbox(
         'Map Style :world_map:',
@@ -131,17 +168,78 @@ def display_user_interaction():
 
     #st.sidebar.checkbox("Disable selectbox widget", key="disabled")
     sample = st.sidebar.radio(
-        "Set dataset sample :eyeglasses:",
+        "Set dataset sample:",
         key="sample",
         options=["train", "test", "validation","full"],
         index=0
     )
 
     return filename, sample
+##### end sidebar user interface functions ##################
+
+##### metrics, charts and maps functions ##################
+def plot_map(df):
+    
+    map = folium.Map(location=[df['lat'].values.mean(), df['lon'].values.mean()], zoom_start=11.5, width='100%',height="100", scrollWheelZoom=True, tiles=st.session_state.map_style)
+    minimap = plugins.MiniMap()
+    map.add_child(minimap)
+    for i in range(0,len(df)):
+
+        
+        if 0 <= df.iloc[i]['score'] < 0.5:
+            color = 'green'
+            classification = 'low'
+        elif 0.5 <= df.iloc[i]['score'] < 0.9:
+            color = 'yellow'
+            classification = 'moderate'
+        else:
+            color = 'red'
+            classification = 'high'
+
+        prediction_accuracy = "false"
+        
+        right = f"""<rect x="7", y="7" width="5" height="5", fill={color}, opacity=".3" /> """
+        if df.iloc[i]['class'] == df.iloc[i]['prediction']:
+            prediction_accuracy = "true"
+            right = f""""""
 
 
-##### user interface functions ##################
+        html=f"""
+            <div style="width: 100%; z-index: 2; border-radius: 10px; border-radius: 4px; color: white; font-size: 12px; background-color: #192733; font-family: courier new">
+            <h5> ANN classification: {classification}</h5>
+            <p style="font-size:10px">Additional information:</p>
+            <ul>
+                <li style="font-size:10px">actual: {df.iloc[i]['class']}</li>
+                <li style="font-size:10px">prediction: {df.iloc[i]['prediction']}</li>
+                <li style="font-size:10px">rightnesses: {prediction_accuracy}</li>
+                <li style="font-size:10px">probability: {'{:.2f}%'.format(df.iloc[i]['score']*100)}</li>
+            </ul>
+            <ul>
+                <li style="font-size:10px">slope: {'{:.3f}'.format(df.iloc[i]['slope'])}</li>
+                <li style="font-size:10px">twi: {'{:.3f}'.format(df.iloc[i]['twi'])}</li>
+                <li style="font-size:10px">aspect: {'{:.3f}'.format(df.iloc[i]['aspect'])}</li>
+                <li style="font-size:10px">elevation: {'{:.3f}'.format(df.iloc[i]['elevation'])}</li>
+                <li style="font-size:10px">curvature: {'{:.3f}'.format(df.iloc[i]['curvature'])}</li>
+                
+            </ul>
+            </p>
+            </div>
+            """
+        
+        iframe = folium.IFrame(html=html, width=175, height=168)
+        popup = folium.Popup(iframe, max_width=2650)
+        folium.Marker(
+            location=[df.iloc[i]['lat'], df.iloc[i]['lon']],
+            #tooltip=folium.Tooltip(iframe.render()),
+            popup=popup,
+            icon=folium.DivIcon(html=f"""
+                <div><svg>
+                    <circle cx="10" cy="10" r="8" fill={color} opacity=".4"/>
+                    {right}
+                </svg></div>""")
+        ).add_to(map)
 
+    return map
 
 
 def plot_classification_pie(source):
@@ -152,15 +250,24 @@ def plot_classification_pie(source):
     ).properties(
         title=f"Prediction classification over {(source['total'].sum())} pinpoints"
     )
-
     pie = base.mark_arc(outerRadius=100)
     text = base.mark_text(radius=140, size=18).encode(text="classification:N")
 
     chart = pie + text
-
-
     return chart
 
+
+def plot_bar_ann_assessment(source):
+
+    chart = alt.Chart(source).mark_bar(
+        cornerRadiusTopLeft=4,
+        cornerRadiusTopRight=4
+    ).encode(
+        x='total:Q',
+        y='classification:O'
+        
+    ).properties(title="ANN assessment")
+    return chart
      
 @st.cache_data
 def plot_horizontal_bar(source):
@@ -180,52 +287,17 @@ def plot_horizontal_bar(source):
     #).transform_window(
     #    rank='row_number()',
     #    sort=[alt.SortField("scores", order="descending")],
-    #).properties(
-    #    title="Feature importance",
-    #)
 
     return chart 
     
+##### end metrics, charts and maps functions ##################
 
 
 
 
-def getdata(fileName=None):
-    #Get data with UTM coordinates and convert them to lat/long
-    landslide_df = pd.read_csv(fileName)
-    zone_number = 23   #São Paulo coast UTM zone number
-    zone_letter = 'L'  #São Paulo coastUTM zone letter
-    landslide_df['lat'], landslide_df['lon'] = utm.to_latlon(easting=landslide_df['X'].values,northing= landslide_df['Y'].values,zone_number=zone_number,zone_letter=zone_letter)
-    
-    return landslide_df
 
 
 
-# Compute and display performance metrics 
-def NetworkPerformance(y_real, y_prob):
-    y_predict = (y_prob > 0.5)
-    # accuracy: (tp + tn) / (p + n)
-    accuracy = accuracy_score(y_real, y_predict)
-    #print('Accuracy: %f' % accuracy)
-    # precision tp / (tp + fp)
-    precision = precision_score(y_real, y_predict)
-    #print('Precision: %f' % precision)
-    # recall: tp / (tp + fn)
-    recall = recall_score(y_real, y_predict)
-    #print('Recall: %f' % recall)
-    # f1: 2 tp / (2 tp + fp + fn)
-    f1 = f1_score(y_real, y_predict)
-    #print('F1 score: %f' % f1)
-    # kappa
-    kappa = cohen_kappa_score(y_real, y_predict)
-    #print('Cohens kappa: %f' % kappa)
-    # ROC AUC
-    auc = roc_auc_score(y_real, y_prob)
-    #print('ROC AUC: %f' % auc)
-    # confusion matrix
-    matrix = confusion_matrix(y_real, y_predict)
-    
-    return accuracy, recall, auc 
 
 
 
@@ -244,96 +316,32 @@ def main():
     
     filename, sample = display_user_interaction()
     
-    #Load and Prepare Data
+    #Load and Prepare Data depending on user selected data file and sample
     df = getdata(fileName = filename)
-    X_train, X_test, X_val, y_train, y_test, y_val, df = dataPreparation(df,sample)
-    X = np.concatenate((X_train, X_val))
-    X = np.concatenate((X, X_test))
-    y = np.concatenate((y_train, y_val))
-    y = np.concatenate((y, y_test))
-
-    
-    if st.session_state.sample == "train":
-        X = X_train
-        y = y_train
-    elif st.session_state.sample == "test":
-        X = X_test
-        y = y_test
-    elif st.session_state.sample == "validation":
-        X = X_val
-        y = y_val
-    
-    y = y.ravel() #convert that array shape to (n, ) (i.e. flatten it)
+    X, y, df = dataPreparation(df)
     
     #Predict landslide based on prepared data and return dataset with computed columns
     df, y_pred  = run_prediction(df,X)
     acc, rcl, auc = NetworkPerformance(y, (y_pred))
 
+    #prepare a dataframe to compute features importance
     df_importance = df[['slope','aspect','elevation','land_use','lithology','twi','curvature']]
-    predictors, scores = features_importance(X=df_importance,y=y)
+    predictors, scores = features_importance(X=df_importance, y=y)
     
     #Display Metrics
     st.subheader(f'Predictions Performance')
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.metric("Accurancy", '{:.2f}%'.format(acc*100))
+        st.metric("Accurancy", value='{:.2f}%'.format(acc*100), help="It is the fraction of predictions our model got right, it is computed by: (tp + tn) / (tp + tn + fp + fn)" )
     with col2:
-        st.metric("Sensibility", '{:.2f}%'.format(rcl*100))
+        st.metric("Sensitivity", value='{:.2f}%'.format(rcl*100), help="Also known as Recall or True positive rate it is computed by: tp / (tp + fn)")
     with col3:
-        st.metric("ROC-AUC", '{:.2f}%'.format(auc*100))
-
-    
-    map = folium.Map(location=[df['lat'].values.mean(), df['lon'].values.mean()], zoom_start=11.5, width='700',height="100", scrollWheelZoom=True, tiles=st.session_state.map_style)
-    minimap = plugins.MiniMap()
-    map.add_child(minimap)
-    for i in range(0,len(df)):
-
-        prediction_accuracy = "false"
-        if df.iloc[i]['class'] == df.iloc[i]['prediction']:
-            prediction_accuracy = "true"
-
-        
-        if 0 <= df.iloc[i]['score'] < 0.5:
-            color = 'green'
-            classification = 'low'
-        elif 0.5 <= df.iloc[i]['score'] < 0.9:
-            color = 'yellow'
-            classification = 'moderate'
-        else:
-            color = 'red'
-            classification = 'high'
-        
-
-
-        html=f"""
-            <h5> ANN classification: {classification}</h5>
-            <p style="font-size:13px">additional information:</p>
-            <ul>
-                <li style="font-size:12px">actual: {df.iloc[i]['class']}</li>
-                <li style="font-size:12px">prediction: {df.iloc[i]['prediction']}</li>
-                <li style="font-size:12px">accuracy: {prediction_accuracy}</li>
-                <li style="font-size:12px">probability: {'{:.2f}%'.format(df.iloc[i]['score']*100)}</li>
-            </ul>
-            </p>
-            """
-        iframe = folium.IFrame(html=html, width=175, height=168)
-        popup = folium.Popup(iframe, max_width=2650)
-        folium.Marker(
-            location=[df.iloc[i]['lat'], df.iloc[i]['lon']],
-            popup=popup,
-            icon=folium.DivIcon(html=f"""
-                <div><svg>
-                    <circle cx="10" cy="10" r="8" fill={color} opacity=".4"/>
-                </svg></div>""")
-        ).add_to(map)
-
-    st_folium(fig=map, width=1200,height=500)
-
+        st.metric("ROC-AUC", value='{:.2f}%'.format(auc*100), help="It correlates the true positive rate (TPR) against the false positive rate (FPR)")
 
      ## Create a map with a specified zoom level
     col4, col5, col6 = st.columns(3)
     
-    ann_prediction_data = pd.DataFrame({'classification': ['right', 'wrong'], 'total': [len(df[df['class'] == df['prediction']]),4]})
+    ann_prediction_data = pd.DataFrame({'classification': ['right', 'wrong'], 'total': [len(df[df['class'] == df['prediction']]),len(df[df['class'] != df['prediction']])]})
 
     pie_data = pd.DataFrame({'classification': ['high', 'moderate', 'low'], 
                              'total': [len(df[df['score']>=0.9]),len(df[(df["score"]>= 0.5) & (df["score"]<=0.9)]),len(df[df['score']<0.5])]})
@@ -342,14 +350,18 @@ def main():
 
     chart_classification = plot_classification_pie(source=pie_data)
     chart_features_importance = plot_horizontal_bar(source=chart_data)
-    chart_prediction_assessment = plot_classification_pie(source=pie_data)
+    chart_prediction_assessment = plot_bar_ann_assessment(source=ann_prediction_data)
 
     
     col4.altair_chart(chart_classification, theme="streamlit", use_container_width=True)
     col5.altair_chart(chart_features_importance, theme="streamlit", use_container_width=True)
     col6.altair_chart(chart_prediction_assessment, theme="streamlit", use_container_width=True)
+
+    map = plot_map(df=df)    
+    st_folium(fig=map, width=1200,height=500)
+
     
-    st.write(df)
+    st.write(df,unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
