075a758f54e23a4c03713a7405a4e53cd6eaa628
Caio Azevedo <cazevedo@hotmail.com>
2024-03-11 11:33:26 -0300

    Add files via upload

--- map.py
+++ map.py
@@ -3,13 +3,11 @@ import pandas as pd
 import numpy as np
 import os
 
-
-
 import Rede_Neural as rna
 from Rede_Neural import NeuralNetwork
 from Rede_Neural import Layer
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
